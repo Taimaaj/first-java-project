9b81123561e38148d09de01174e47a6c34a6cdc0
Taimaaj <110972649+Taimaaj@users.noreply.github.com>
2022-08-21 15:42:56 +0300

    Create  FuelType Enum

---
+++ VehaiclesCenter/FuelType.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class FuelType - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum FuelType
+{
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
