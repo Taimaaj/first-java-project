0b49af46522814bbd9a19e11b6555adf68203d15
Taimaaj <110972649+Taimaaj@users.noreply.github.com>
2022-08-21 15:46:22 +0300

    Create  GearType Enum

---
+++ VehaiclesCenter/GearType.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class GearType - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum GearType
+{
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
