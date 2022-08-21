2984bc48db1e3e43589234c5e0a548dcb26c645e
Taimaaj <110972649+Taimaaj@users.noreply.github.com>
2022-08-21 15:47:45 +0300

    I filled GearType Enum with his data

--- VehaiclesCenter/GearType.java
+++ VehaiclesCenter/GearType.java
@@ -1,11 +1,5 @@
-
-/**
- * Enumeration class GearType - write a description of the enum class here
- *
- * @author (your name here)
- * @version (version number or date here)
- */
 public enum GearType
 {
-    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+     Normal ,Automatic 
+
 }
