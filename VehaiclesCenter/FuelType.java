2f933b43905651963212ac1b0ef9996b1480616d
Taimaaj <110972649+Taimaaj@users.noreply.github.com>
2022-08-21 15:45:14 +0300

    I filled FuelType Enum with his data

--- VehaiclesCenter/FuelType.java
+++ VehaiclesCenter/FuelType.java
@@ -1,11 +1,4 @@
-
-/**
- * Enumeration class FuelType - write a description of the enum class here
- *
- * @author (your name here)
- * @version (version number or date here)
- */
 public enum FuelType
 {
-    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+   Diesel ,Gasoline
 }
