0f1b534990c40692152a783c8bb565e5dd067fb2
Taimaaj <110972649+Taimaaj@users.noreply.github.com>
2022-08-23 21:17:41 +0300

    Create some edits on code style in Automobile class

--- VehaiclesCenter/Automobile.java
+++ VehaiclesCenter/Automobile.java
@@ -3,28 +3,28 @@ import java.util.Date;
 public class Automobile
 {
     Engine e; 
-    public String Manufacture_Company;
-    public  Date Manufacture_date;
+    public String manufactureCompany;
+    public  Date manufactureDate;
     public String model ;
-    public int Plate_num ;
-    public int Body_serail_num;
+    public int plateNum ;
+    public int bodySerailNum;
     
     public Automobile()
     {
-        Manufacture_Company=null;
-        Manufacture_date=null;
-        model=null;
-        Plate_num=0;
-        Body_serail_num=0;
+       manufactureCompany = null;
+      manufactureDate = null;
+        model = null;
+        plateNum = 0;
+       bodySerailNum = 0;
     }
     
-    public Automobile(String Manufacture_Company, Date Manufacture_date,String model , int Plate_num,int Body_serail_num )
+    public Automobile(String manufactureCompany ,Date manufactureDate,String model , int  plateNum ,int bodySerailNum )
     {
-          Manufacture_Company=Manufacture_Company;
-          Manufacture_date=Manufacture_date;
+      manufactureCompany = manufactureCompany;
+      manufactureDate=manufactureDate;
           model=model;
-          Plate_num= Plate_num;
-        Body_serail_num= Body_serail_num;
+          plateNum=  plateNum;
+        bodySerailNum= bodySerailNum;
     }
     
     
