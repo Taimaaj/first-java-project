fbf3a32dbbab62e39c969983d30dcce337c51614
Taimaaj <110972649+Taimaaj@users.noreply.github.com>
2022-08-29 23:30:41 +0300

    Create some edits in  Engine class

--- VehaiclesCenter/Engine.java
+++ VehaiclesCenter/Engine.java
@@ -1,35 +1,27 @@
 import java.util.Date;
-public class Engine
-{
-    public String  manufactur;  
-    public  Date  manufactureDate;
-    public String  model;
-    public int capacity;
-    public  int  cylinders;
-    
-    public Engine()
-    {
+    public class Engine{
+    public String     manufactur;  
+    public Date       manufactureDate;
+    public String     model;
+    public int        capacity;
+    public int        cylinders;
+    public FuelType   FuelType;
+
+    public Engine() {
          manufactur= null;
          manufactureDate=null;
-         model=null;
+         FuelType model=null;
          capacity=0;
          cylinders=0;
     }
     
-    
-     public Engine(String  manufactur, Date  manufactureDate,Date model,int capacity,int cylinders)
-     {
-       manufactur= manufactur;
-       manufactureDate= manufactureDate;
-        model=model;
-        capacity= capacity;
-        cylinders= cylinders;
+     public Engine(String  manufactur, Date  manufactureDate,Date model,int capacity,int cylinders){
+       this.manufactur= manufactur;
+       this.manufactureDate= manufactureDate;
+       model=model;
+       this. capacity= capacity;
+       this. cylinders= cylinders;
 
-    } 
-    
-    
-    
-    
-    
-}
+    }   
+    }
 
