bca8d15ae41330fbe04580c01b24d7bc5a7b58a3
Taimaaj <110972649+Taimaaj@users.noreply.github.com>
2022-08-29 23:41:13 +0300

    Create some edits in  Car  class

--- VehaiclesCenter/Car.java
+++ VehaiclesCenter/Car.java
@@ -1,35 +1,31 @@
 
-public class Car extends Vehicle
-{
-    public int chairNum;
+    public class Car extends Vehicle{
+    public  int      chairNum;
     public boolean  isFurnitreLeather;
  
-     public Car()
-    {
-        chairNum=0;
-        isFurnitreLeather =false;
-        length=0;
-       width=0;
-       color=null;
-        manufactureCompany = null;
-      manufactureDate = null;
-        model = null;
-        plateNum = 0;
-       bodySerailNum = 0;
+     public Car(){
+       chairNum = 0;
+       isFurnitreLeather = false;
+       length = 0;
+       width = 0;
+       color = "";
+       manufactureCompany = "" ;
+       manufactureDate = null;
+       bodySerailNum = "";
+       model =  ""  ;
+       plateNum  =   "" ;
     }
     
-    public Car( int chairNum,boolean  isFurnitreLeather) 
-    {
-     chairNum = chairNum ;
-      isFurnitreLeather =isFurnitreLeather ;
-      length=length;
-       width=width;
-       color=color;
-      manufactureCompany = manufactureCompany;
-      manufactureDate=manufactureDate;
-          model=model;
-          plateNum=  plateNum;
-        bodySerailNum= bodySerailNum;
+    public Car( int chairNum,boolean  isFurnitreLeather) {
+      this.chairNum = chairNum ;
+      this.isFurnitreLeather =  isFurnitreLeather ;
+      this.length = length;
+      this.width = width;
+      this.color = color;
+      this.manufactureCompany = manufactureCompany;
+      this.manufactureDate = manufactureDate;
+      this.model = model;
+      this.plateNum = plateNum;
+      this.bodySerailNum =  bodySerailNum;
     }
-    
-}
+    }
