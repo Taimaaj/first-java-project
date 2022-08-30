5a7db509394faa7955859fdc86a67a65f3debeea
Taimaaj <110972649+Taimaaj@users.noreply.github.com>
2022-08-30 18:18:41 +0300

    Edites on Automobile class

--- VehaiclesCenter/Automobile.java
+++ VehaiclesCenter/Automobile.java
@@ -1,25 +1,23 @@
 import java.util.Date;
-public class Automobile {
-    Engine Engine; 
-    
-    public  String  manufactureCompany;
-    public  String  model ;
-    public  String  plateNum ;
-    public  String  bodySerailNum;
-    public  Date    manufactureDate;
-
+    public  class Automobile {
+    public  Engine    engine; 
+    public  String    manufactureCompany;
+    public  String    model ;
+    public  String    plateNum ;
+    public  String    bodySerailNum;
+    public  Date      manufactureDate;
+    public  GearType  gearType;
     public Automobile(){
-       manufactureCompany =notdecided          ;
-       manufactureDate    = 0/0/0000;
-       bodySerailNum   =            ;
-       model  =  null               ;
-       plateNum  =                  ;
+       manufactureCompany ="notdecided";
+       manufactureDate = null;
+       bodySerailNum = "notdecided";
+       model = "notdecided" ;
     }
     public Automobile(String manufactureCompany ,Date manufactureDate,String model ,String plateNum ,String bodySerailNum ){
-      manufactureCompany = manufactureCompany;
-      manufactureDate=manufactureDate;
-      model=model;
-      plateNum=  plateNum;
-      bodySerailNum= bodySerailNum;
+      this.manufactureCompany = manufactureCompany;
+      this.manufactureDate = manufactureDate;
+      this.model = model;
+      this.plateNum = plateNum;
+      this.bodySerailNum =  bodySerailNum;
     } 
     }
