d552069e05abbc33b636572dea5ec14ee9693630
Taimaaj <110972649+Taimaaj@users.noreply.github.com>
2022-08-30 18:35:16 +0300

    Edits on car class

--- VehaiclesCenter/Car.java
+++ VehaiclesCenter/Car.java
@@ -1,21 +1,17 @@
-
     public class Car extends Vehicle{
     public  int      chairNum;
-    public boolean  isFurnitreLeather;
- 
-     public Car(){
+    public  boolean  isFurnitreLeather;
+      public Car(){
        chairNum = 0;
        isFurnitreLeather = false;
        length = 0;
        width = 0;
-       color = "";
-       manufactureCompany = "" ;
+       color = "notdecided";
+       manufactureCompany ="notdecided";
        manufactureDate = null;
-       bodySerailNum = "";
-       model =  ""  ;
-       plateNum  =   "" ;
+       bodySerailNum = "notdecided";
+       model = "notdecided" ;
     }
-    
     public Car( int chairNum,boolean  isFurnitreLeather) {
       this.chairNum = chairNum ;
       this.isFurnitreLeather =  isFurnitreLeather ;
