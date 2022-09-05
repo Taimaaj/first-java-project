427e30deda6a78a15aff6a84af46c4767326a555
Taimaaj <110972649+Taimaaj@users.noreply.github.com>
2022-09-05 18:36:59 +0300

    Create edits on Vehicle class

--- VehaiclesCenter/Car.java
+++ VehaiclesCenter/Car.java
@@ -1,22 +1,22 @@
-import java.util.Date;
-
+    import java.util.Date;
     public class Car extends Vehicle{
     public  int      chairNum;
     public  boolean  isFurnitreLeather;
       public Car(){
-       chairNum = 0;
+       chairNum = 5;
        isFurnitreLeather = false;
-       length = 0;
-       width = 0;
+       length = 0.0;
+       width = 0.0;
        color = "White";
        manufactureCompany ="notdecided";
        manufactureDate = new Date(01-01-1970);
        bodySerailNum = "notdecided";
        model = "notdecided" ;
+       plateNum = "notdecided";
     }
-    public Car( int chairNum,boolean  isFurnitreLeather) {
+    public Car(int chairNum,boolean isFurnitreLeather){
       this.chairNum = chairNum ;
-      this.isFurnitreLeather =  isFurnitreLeather;
+      this.isFurnitreLeather = isFurnitreLeather;
       this.length = length;
       this.width = width;
       this.color = color;
@@ -24,6 +24,6 @@ import java.util.Date;
       this.manufactureDate = manufactureDate;
       this.model = model;
       this.plateNum = plateNum;
-      this.bodySerailNum =  bodySerailNum;
+      this.bodySerailNum = bodySerailNum;
     }
     }
