a58c59c1aaec483ea5a74f219d9778dc426d213c
Taimaaj <110972649+Taimaaj@users.noreply.github.com>
2022-09-05 18:25:27 +0300

    Create edits on Motorcycle class

--- VehaiclesCenter/Motorcycle.java
+++ VehaiclesCenter/Motorcycle.java
@@ -1,18 +1,17 @@
-import java.util.Date;
-
+     import java.util.Date;
      public class Motorcycle extends Automobile{
      public  double tierDiameter;
      public  double length;
      public Motorcycle(){
        tierDiameter = 0.0;
        length = 0.0;
-       manufactureCompany ="notdecided";
+       manufactureCompany = "notdecided";
        manufactureDate = new Date(01-01-1970);
        bodySerailNum = "notdecided";
        model = "notdecided" ;
+       plateNum = "notdecided";
      }
-     public Motorcycle(double tierDiameter,double length)
-     {
+     public Motorcycle(double tierDiameter,double length){
       this.tierDiameter = tierDiameter;
       this.length = length;
       this.manufactureCompany = manufactureCompany;
