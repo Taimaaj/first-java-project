200cc3efb12874f6931886431d913e73c20f882b
Taimaaj <110972649+Taimaaj@users.noreply.github.com>
2022-12-22 15:57:02 +0200

    create some edits on main class

--- VehaiclesCenter/Main.java
+++ VehaiclesCenter/Main.java
@@ -1,16 +1,17 @@
     import java.util.Scanner;
     import java.lang.String;
     import java.util.*; 
+    import java.time.LocalDate;
     public class Main{
     public static void main(String args[])  {
-       Car carObj = new Car();
-       Truck truckObj = new Truck();
-       Motorcycle motorcycleObj = new Motorcycle();
+       List<Car> carList = new ArrayList<Car>();
+       List<Truck> truckList = new ArrayList<Truck>();
+       List<Motorcycle> MotorcycleList = new ArrayList<Motorcycle>();
        System.out.println("                           🏍 🚚 🚗 Welcom in vechicles world 🚗 🚚 🏍️    ");
        System.out.println("please Enter your name");
-       Scanner namee = new Scanner (System.in);
-       String name=namee.nextLine();
-       System.out.println("Welcome dear "+name);
+       Scanner username = new Scanner (System.in);
+       String name = username.nextLine();
+       System.out.println("Welcome dear "+username);
        System.out.println("____________________________________________________________________________________________");
        System.out.println("Enter Vechicle type");
        Scanner in = new Scanner ( System.in );
@@ -19,42 +20,86 @@
        switch (in.nextInt() ) {
        case 1:
         System.out.println ( "You picked Car" );
-        List<Car> carList = new ArrayList<Car>();
+        Car carObj = new Car();
+        Engine engineObj = new Engine();
         for (int i = 0; i < 1; i++) {
+            //scanner for car class
             Scanner car = new Scanner(System.in);
             System.out.println("Let's add a new car to the vehicle center:");
-            System.out.println("\n>Please enter chairNum of the car: ");
-            int chairNum = car.nextInt();
-            System.out.println(">Please enter the length of car : ");
-            int length = car.nextInt();
-            System.out.println(">Please enter the width of car: ");
-            int width = car.nextInt();
-            System.out.println(">Please enter the color of car: ");
-            String color = car.next();
-            System.out.println(">Please enter the manufactureCompany of car: ");
-            String manufactureCompany = car.next();
-            System.out.println(">Please enter if isFurnitreLeather of car: ");
-           // boolean isFurnitreLeather = car.nextBoolean();
+            System.out.println(">Please enter the manufactur of the engine: ");
+            engineObj.manufactur = car.next();
+            System.out.println(">Please enter the manufactureDate ofthe engine: ");
+            String manufactureDate = car.nextLine();
+            LocalDate _manufactureDate  = LocalDate.parse(manufactureDate);
+            System.out.println(">Please enter the model of the engine: ");       
+            engineObj. model = car.next();
+            System.out.println(">Please enter the capacity of the engine: ");
+            engineObj.capacity = car.nextInt();
+            System.out.println(">Please enter the cylinders of the engine: ");
+            engineObj.cylinders = car.nextInt();
+            System.out.println(">Please enter the FuelType of the engine: ");
+            engineObj.fuelType = car.nextInt();
+            System.out.println ( "1) Diesel(1)\n2)Gasoline(2)\n " );
+            System.out.print ( "Selection: " );
+            switch (in.nextInt() ) {
+              case 1:
+                  System.out.println ( "You picked Diesel" );    
+                break;
+              case 2:
+               System.out.println ( "You picked Gasoline" );
+                break;
+               default:
+               System.out.println ( "Unrecognized option" );
+              }
+             System.out.println(">Please enter the manufactureCompany of car: ");
+             carObj. manufactureCompany = car.next();
+             System.out.println(">Please enter the plateNum of car: ");
+             carObj. plateNum = car.next();
+             System.out.println(">Please enter the bodySerailNum of car: ");
+             carObj .bodySerailNum = car.next();
+             System.out.println(">Please enter the manufactureDate of car: ");
+             String manufacturedate = car.nextLine();
+             LocalDate _manufacturedate  = LocalDate.parse(manufactureDate);         
+             System.out.println(">Please enter the length of car : ");
+             carObj .length = car.nextInt();
+             System.out.println(">Please enter the width of car: ");
+             carObj. width = car.nextInt();
+             System.out.println(">Please enter the color of car: ");
+             String color = car.next(); 
+             System.out.println(">Please enter the GearType of car: ");
+             carObj.gearType = car.nextInt();
+             System.out.println ( "1) Normal(1)\n2)Automatic(2)\n " );
+             System.out.print ( "Selection: " );
+             switch (in.nextInt() ) {
+              case 1:
+                  System.out.println ( "You picked Normal" );    
+                break;
+              case 2:
+               System.out.println ( "You picked Automatic" );
+                break;
+              default:
+               System.out.println ( "Unrecognized option" );
+              }
+             System.out.println("\n>Please enter chairNum of the car: ");
+             carObj.chairNum= car.nextInt();
+              while (true) {
+             System.out.println(">Please enter if isFurnitreLeather of car: ");
+             String _isFurnitreLeather = car.nextLine();
+             try {
+                boolean isFurnitreLeather = Boolean.parseBoolean(_isFurnitreLeather);
+                carObj.setisFurnitreLeather(isFurnitreLeather);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid input. Please try again.");
+            }}
             System.out.println(">Please enter the manufactureDate of car: ");
-            int manufactureDate = car.nextInt();
-            System.out.println(">Please enter the bodySerailNum of car: ");
-            int bodySerailNum = car.nextInt();
-            System.out.println(">Please enter the model of car: ");       
-            String model = car.next();
-            carObj.chairNum=chairNum;
-            carObj.length=length;
-            carObj.width=width;
-            carObj.color=color;
-            carObj.manufactureCompany=manufactureCompany;
-            //carObj.manufactureDate=manufactureDate;
-            // carObj.bodySerailNum=bodySerailNum;
-            carObj.model=model;
+
             System.out.println("You have successfully added a new Car to the vehicle center!");
             }
-         break;
-       case 2:
+        
+    
+      /* case 2:
         System.out.println ( "You picked  Truck" );
-        List<Truck> truckList = new ArrayList<Truck>();
         for (int i = 0; i < 1; i++) {
             Scanner truck = new Scanner(System.in);
             System.out.println("Let's add a new Truck to the vehicle center:");
@@ -87,7 +132,6 @@
          break;
        case 3:
          System.out.println ( "You picked  Motorcycle" );
-         List<Motorcycle> MotorcycleList = new ArrayList<Motorcycle>();
          for (int i = 0; i < 1; i++) {
             Scanner Motorcycle= new Scanner(System.in);
             System.out.println("Let's add a new MotorcycleList to the vehicle center:");
@@ -114,10 +158,9 @@
          break;
        default:
         System.out.println ( "Unrecognized option" );
-        break;
+        break;*/
+    }}}
 
-}
-}
-}
+
    
     
