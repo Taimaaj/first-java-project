9cdfd5f4037057571cbe7190d8400806c2530ef7
Taimaaj <110972649+Taimaaj@users.noreply.github.com>
2022-12-25 23:59:41 +0200

    edits on the class

--- VehaiclesCenter/Engine.java
+++ VehaiclesCenter/Engine.java
@@ -1,20 +1,20 @@
     import java.util.Date;
     public class Engine{
     public  String     manufactur;  
-    public  Date       manufactureDate;
+    public  String     manufactureDate;
     public  String     model;
     public  int        capacity;
     public  int        cylinders;
-    private FuelType   fuelType;
+    public FuelType   fuelType;
     public Engine() {
          manufactur = "notdecided";
-         manufactureDate = new Date(01-01-1970);
+         manufactureDate ="";
          model = "notdecided";
          capacity = 0;
          cylinders = 0;
          fuelType = fuelType.UNDEFINED;
         }
-    public Engine(String  manufactur, Date  manufactureDate,String model,int capacity,int cylinders,FuelType fuelType){
+    public Engine(String  manufactur, String  manufactureDate,String model,int capacity,int cylinders,FuelType fuelType){
        this.manufactur = manufactur;
        this.manufactureDate = manufactureDate;
        this.model = model;
@@ -22,5 +22,50 @@
        this.cylinders = cylinders;
        this.fuelType = fuelType;
     }   
+    public String getmanufactur() {
+        return manufactur;
     }
-
+    public void setManufacture(String manufacture) {
+        this.manufactur = manufacture;
+    }
+    public String getManufactureDate() {
+        return manufactureDate;
+    }
+    public void setManufactureDate(String manufactureDate) {
+        this.manufactureDate = manufactureDate;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public int getCapacity() {
+        return capacity;
+    }
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+    public int getCylinders() {
+        return cylinders;
+    }
+    public void setCylinders(int cylinders) {
+        this.cylinders = cylinders;
+    }
+    public FuelType getFuelType() {
+        return fuelType;
+    }
+    public void setFuelType(FuelType fuelType) {
+        this.fuelType = fuelType;
+    }
+    public void print() {
+    System.out.println("--- Engine ---");
+    System.out.println("Manufacture: " + this.getmanufactur());
+    System.out.println("Manufacture date: " + this.getManufactureDate());
+    System.out.println("Model: " + this.getModel());
+    System.out.println("Capacity: " + this.getCapacity());
+    System.out.println("Cylinders: " + this.getCylinders());
+    System.out.println("Fuel type: " + this.getFuelType());
+    }
+    }  
+    
